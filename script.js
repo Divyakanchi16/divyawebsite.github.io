021f686ba9eca3a77f10473bbaad960b1d691b39
Divyakanchi16 <147559710+Divyakanchi16@users.noreply.github.com>
2023-12-05 23:32:31 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function processImage() {
+    // Add image processing logic here
+    alert('Image processing logic goes here!');
+}
+
+function submitConcerns() {
+    // Add concerns submission logic here
+    alert('Concerns submitted successfully!');
+}
